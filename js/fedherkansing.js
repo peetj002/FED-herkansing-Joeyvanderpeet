c98b8921a16afb6674eb93c5f25034d5c291dada
Joey <joey.van.der.peet@hva.nl>
2020-01-05 16:21:14 +0100

    Marginleft 5% en video kloppend

--- js/fedherkansing.js
+++ js/fedherkansing.js
@@ -10,4 +10,35 @@ function klapHamburgeruit() {
 
 Hamburgericon.addEventListener('click', klapHamburgeruit);
 
+// Filteren en sorteren tablet/desktop
+// Sorteren
+
+var ButtonAZ = document.querySelector('.filterensorterendesktop button:first-of-type');
+var Formulier = document.querySelector('#Sorterenplaatsing');
+var Pijltje = document.querySelector('.filterensorterendesktop img:nth-of-type(1)');
+var Verhalen = document.querySelector('#articles');
+
+function klapSorterenuit() {
+    ButtonAZ.classList.toggle('active');
+    Formulier.classList.toggle('active');
+    Pijltje.classList.toggle('active');
+    Verhalen.classList.toggle('active');
+}
+
+ButtonAZ.addEventListener('click', klapSorterenuit);
+
+// Categorieën
 //
+//var  = document.querySelector('.filterensorterendesktop button:first-of-type');
+//var Formulier = document.querySelector('#Sorterenplaatsing');
+//var Pijltje = document.querySelector('.filterensorterendesktop img:nth-of-type(1)');
+//var Verhalen = document.querySelector('#articles');
+//
+//function klapCategorieenuit() {
+//    Buttoncategorie.classList.toggle('active');
+//    Formulier.classList.toggle('active');
+//    Pijltje.classList.toggle('active');
+//    Verhalen.classList.toggle('active');
+//}
+//
+//Buttoncategorie.addEventListener('click', klapCategorieenuit);
